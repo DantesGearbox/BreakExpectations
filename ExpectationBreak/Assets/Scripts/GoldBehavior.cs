51697bd205dfb3ff07d30174414baba1cb979201
DantesGearbox <mads.engberg@hotmail.com>
2019-02-23 21:46:42 +0100

    Added a gold thing

---
+++ ExpectationBreak/Assets/Scripts/GoldBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldBehavior : MonoBehaviour{
+
+	ParticleSystem ps;
+	SpriteRenderer sr;
+
+	bool dead = false;
+
+	// Start is called before the first frame update
+	void Start() {
+		sr = GetComponent<SpriteRenderer>();
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+
+		if (!dead) {
+			sr.enabled = false;
+			ps.Play();
+			dead = true;
+		}
+
+	}
+}
