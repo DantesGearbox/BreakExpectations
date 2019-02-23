ceda37852b3ee4bc2fee29478160205a3ae77a9d
DantesGearbox <mads.engberg@hotmail.com>
2019-02-23 13:55:01 +0100

    Inital project

---
+++ ExpectationBreak/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour{
+
+	public BoxCollider2D bc2;
+
+	// Start is called before the first frame update
+	void Start() {
+		transform.localScale = new Vector3(0.5f, 5, 1);
+	}
+	
+    // Update is called once per frame
+	void Update() {
+		transform.position += new Vector3(0.01f, 0, 0);
+	}
+}
