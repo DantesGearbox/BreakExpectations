34a887a1efe0f1387e2308c8f33536618cdf6867
DantesGearbox <mads.engberg@hotmail.com>
2019-02-23 16:27:36 +0100

    Stuffffff

--- ExpectationBreak/Assets/Scripts/Spikes.cs
+++ ExpectationBreak/Assets/Scripts/Spikes.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehavior : MonoBehaviour{
-
-	public BoxCollider2D bc2;
-
+public class Spikes : MonoBehaviour{
+	
 	// Start is called before the first frame update
 	void Start() {
-		transform.localScale = new Vector3(0.5f, 5, 1);
+		
 	}
 	
     // Update is called once per frame
 	void Update() {
-		transform.position += new Vector3(0.01f, 0, 0);
+		
 	}
 }
