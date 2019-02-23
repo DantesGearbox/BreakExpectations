b91ee2f6154a1f93c1455c5cf28ba26e6e7fb533
DantesGearbox <mads.engberg@hotmail.com>
2019-02-23 22:12:53 +0100

    More stuff

---
+++ ExpectationBreak/Assets/Scripts/PlayerResetLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerResetLevel : MonoBehaviour{
+	
+	public Transform resetTransform;	
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.gameObject.tag == "DangerSpikes") {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
